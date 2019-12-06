799c6ff0a162cf5f5de1a405ea1f2b92b9b4f95f
Dariusz Okonski <darek200180@gmail.com>
2019-12-06 12:32:24 +0000

    add testing code for class Draw

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // link do gry: https://websamuraj.pl/examples/js/gra/ 
 import Wallet from './Wallet.js';
 import Statistics from './Statistics.js';
+import Draw from './Draw.js';
 
-const statistics = new Statistics();
-
-console.log(statistics.showGameStatistics());
+const draw = new Draw();
+console.log(draw);
+console.log(draw.drawResult());
