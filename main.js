68e3a440439aabb8665286b93ab405eb9395448f
Dariusz Okonski <darek200180@gmail.com>
2019-12-06 12:02:25 +0000

    add code for testing class Statistics

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // link do gry: https://websamuraj.pl/examples/js/gra/ 
 import Wallet from './Wallet.js';
+import Statistics from './Statistics.js';
 
-const wallet = new Wallet(100);
+const statistics = new Statistics();
+
+console.log(statistics.showGameStatistics());
